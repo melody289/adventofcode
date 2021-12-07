cab8727aa24940ffb7db697997540c8c47aaeee0
Melody Walker <melody.walker@ufl.edu>
2021-12-07 14:39:58 -0500

    scrape code

---
+++ data.py
@@ -0,0 +1,18 @@
+
+
+with open('input.txt') as file:
+    data = []
+    for line in file:
+        data.append(int(line))
+
+
+
+
+
+ #        # data = [int(line) for line in file]
+ #
+ # with open('input2.txt')  as file:
+ #     data = []
+ #     for line in file:
+ #         split = line.split()
+ #         data.append((split[0], int(split[1])))
